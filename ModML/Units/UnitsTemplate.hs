bfbc2bfbb9fbb069cf4a8ec619fb74e8c558e002
Andrew <andrew@amxl.com>
2010-03-02 13:37:16 +1300

    Re-arrange the directory structure to a more easily manageable module based structured.

---
+++ ModML/Units/UnitsTemplate.hs
@@ -0,0 +1,13 @@
+module UnitsTemplate
+where
+  -- Numerics support. Rather than using 
+  data OneType = OneType
+  data TwoType = TwoType
+  data ThreeType = ThreeType
+  data FourType = FourType
+  data FiveType = FiveType
+  data SixType = SixType
+  data SevenType = SevenType
+  data EightType = EightType
+  data NineType = NineType
+
